5d14d0a9d4446692d8f22c8294526b3f1357a0d1
willardnyamombe <nya19001@byui.edu>
2021-05-03 20:50:30 -0600

    added week02 and normalization for css

---
+++ week03/js/menu-inclass.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+    const hambuttom =document.queruSelector('.ham');
+    const mainnav = document.querySelector('#navigation');
+    
+    hambutton.addEventListener('click', () => {
+        mainnav.classList.toggle('responsive')},false);
+});

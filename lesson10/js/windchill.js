54bf7e9d68f99eb835180bc3b88283f4df7125c9
willardnyamombe <nya19001@byui.edu>
2021-07-10 20:23:12 -0600

    updating preston

--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -1,4 +1,22 @@
+// weather summary
 
+async function getWeather() {
+
+    const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=953894ad13f71540f4d4ee5b185b63d7&units=imperial'; // url for the weather data api
+    fetch(apiURL)
+        .then((response) => response.json())
+        .then((jsObject) => {
+            console.log(jsObject);
+
+            document.getElementById('condition').textContent = jsObject.weather[0].main;
+            document.getElementById('temp').textContent = Math.round(jsObject.main.temp_max);
+            document.getElementById('windChill').textContent = calcWindChill(jsObject.main.temp_max, jsObject.wind.speed);
+            document.getElementById('humidity').textContent = Math.round(jsObject.main.humidity);
+            document.getElementById('windSpeed').textContent = Math.round(jsObject.wind.speed);
+
+
+
+        })
     const calcWindChill = (temperature, speed) => {
         if (temperature <= 50 && speed > 3) {
             return Math.round(
@@ -9,10 +27,10 @@
         }
     }
 
-    const displayWindChill = () => {
-        let temperature = parseInt(document.getElementById("high").textContent);
-        let wind = parseInt(document.getElementById("windspeed").textContent);
-        let result = calcWindChill(temperature, wind);
-        document.getElementById("windchill").innerHTML = result;
-    }
-    displayWindChill();
+    // const displayWindChill = () => {
+    //     let temperature = parseInt(document.getElementById("high").textContent);
+    //     let wind = parseInt(document.getElementById("windspeed").textContent);
+    //     let result = calcWindChill(temperature, wind);
+    //     document.getElementById("windchill").innerHTML = result;
+    // }
+    // displayWindChill();

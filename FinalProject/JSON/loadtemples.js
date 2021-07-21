f62fdb4b652e70e793a6a08c88f845608e23307b
willardnyamombe <nya19001@byui.edu>
2021-07-21 02:45:48 -0600

    temples page

--- FinalProject/JSON/loadtemples.js
+++ FinalProject/JSON/loadtemples.js
@@ -1,5 +1,26 @@
 window.addEventListener('load' ,()=>{
-    const requestURL = "temples.json"
+
+// Javascript for Footer copyright and date information
+var d = new Date();
+const week = document.querySelector('#weekday');
+var weekday = daysOfWeek(d);
+week.textContent = weekday;
+
+const dayNum = document.getElementById('day');
+dayNum.textContent = d.getDate();
+
+var monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const monthNum = document.getElementById('month');
+monthNum.textContent = monthName[d.getMonth()];
+
+const year = document.getElementById('year');
+year.textContent = d.getFullYear();
+
+const footer = document.querySelector("#copyrightyear");
+footer.textContent = d.getFullYear();
+
+//javascript for temple api
+    const requestURL = "js/temples2.json"
     fetch(requestURL)
     .then((response)=> {
         return response.json();
@@ -24,4 +45,18 @@ function buildTempleCard(temple){
                     <p>Current Presidents: <b>${temple.presidents[temple.presidents.length-1]}</b></p>`;
     document.querySelector("#temples").appendChild(card);
 
+}
+// Function that creates returns the name of the weekday
+function daysOfWeek(date) {
+
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    return weekday[date.getDay()];
 }
